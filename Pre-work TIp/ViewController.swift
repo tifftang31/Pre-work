94888d4f50e2c767f618f6773dcddf4e1509c86c
Tiffany <tiffany@n233-234.mtholyoke.edu>
2018-09-12 18:44:32 -0400

    Initial Commit

---
+++ Pre-work TIp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pre-work TIp
+//
+//  Created by Tiffany on 9/12/18.
+//  Copyright © 2018 Tiffany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
